402ce8c927ab220269595b4611bedf61296cc54c
bhuvi904 <169036689+bhuvi904@users.noreply.github.com>
2025-04-28 20:33:15 +0530

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int l=2,w=5;
+    int area = l*w;
+    cout << area << "\n";
+    return 0;
+}
---
+++ boolean expl1.cpp
@@ -0,0 +1,12 @@
+// write a code on voting age limit 18
+#include <iostream>
+using namespace std;
+int main() {
+    int myage;
+    int votingage=18;
+    cout << "my age =";
+    cin >> myage;
+    cout << (myage >= votingage) << "\n";
+    return 0;
+    
+}
